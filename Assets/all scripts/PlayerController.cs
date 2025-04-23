7e16a133d0db624b2c9ee24ddf1640472f9e73bb
reaperr-xx <supermaksmar4uk324@gmail.com>
2025-04-23 17:34:49 +0300

    PRIZHOKKKKKKKKK!!!!!!!

--- Assets/all scripts/PlayerController.cs
+++ Assets/all scripts/PlayerController.cs
@@ -7,24 +7,28 @@ public class PlayerController : MonoBehaviour
     public float speed;
 
     private Rigidbody2D rb;
-    [SerializeField] public  groundChecker groundcheck;
+    public groundChecker groundcheck;
     public float jump;
 
     void Start()
     {
 
         rb = GetComponent<Rigidbody2D>();
-        groundcheck = GetComponent<groundChecker>();
+        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        groundcheck.GetComponent<groundChecker>();
+        groundChecker grch = groundcheck.GetComponent<groundChecker>();
+
         Vector3 playerInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, 0);
         transform.position = transform.position + playerInput.normalized * speed * Time.deltaTime;
 
 
-        if (Input.GetKeyDown(KeyCode.Space) && groundcheck.isGround == true)
+        if (Input.GetKeyDown(KeyCode.Space) && grch.isGround == true)
         {
             rb.AddForce(new Vector2(rb.velocity.x, jump));
 
